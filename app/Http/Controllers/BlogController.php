040efa2db128c34a5b522bc311c96dda78c37289
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-07-01 10:25:13 +0200

    add post controller model migration

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class BlogController extends Controller
+{
+    public function index(){
+        $posts = Post::all();
+
+        return view('posts.index',[
+            'posts' => $posts
+        ]);
+    }
+}
